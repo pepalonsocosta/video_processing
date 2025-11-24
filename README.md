5ca8412c5f40eceb2f3d2181a4abce45ab4afedd
Pep Alonso <palonso@hubtype.com>
2025-11-24 20:05:07 +0100

    feat(ex-2): implement Seminar 1 with methods for converting rbg to yub and viceversa

--- README.md
+++ README.md
@@ -8,3 +8,23 @@ For this first exercise we just installed ffmpeg, in this case in linux using ap
 by running it on the terminal we get this result:
 
 ![ex1](./s1/exercise1.png)
+
+## ex-2
+
+For this second exeercise we implemented the Seminar1 class with the methods:
+
+- rgb_to_yuv: transforms from RGB space to YUV
+
+- yuv_to_rgb: the inverse transformation
+
+We declared this methods using the @staticmethod decorator to avoid using self notation
+
+For running you can just run the python file (rgb values can be edited on the same file)
+
+using uv that woud be:
+
+```python
+uv run first_seminar.py
+```
+
+uv will create a virtual environment to run the file on it
---
+++ s1/first_seminar.py
@@ -0,0 +1,37 @@
+class Seminar1:
+
+    @staticmethod
+    def rgb_to_yuv(r, g, b):
+        
+        y = 0.257 * r + 0.504 * g + 0.098 * b + 16
+        u = -0.148 * r - 0.291 * g + 0.439 * b + 128
+        v = 0.439 * r - 0.368 * g - 0.071 * b + 128
+        return y, u, v
+
+    @staticmethod
+    def yuv_to_rgb(y, u, v):
+
+        y_prime = y - 16
+        u_prime = u - 128
+        v_prime = v - 128
+        
+        r = 1.164 * y_prime + 1.596 * v_prime
+        g = 1.164 * y_prime - 0.813 * v_prime - 0.391 * u_prime
+        b = 1.164 * y_prime + 2.018 * u_prime
+
+        r = int(round(r))
+        g = int(round(g))
+        b = int(round(b))
+
+        return r, g, b
+
+
+# ex-2
+
+r, g, b = 120, 200, 80
+y, u, v = Seminar1.rgb_to_yuv(r, g, b)
+print("YUV:", y, u, v)
+
+r2, g2, b2 = Seminar1.yuv_to_rgb(y, u, v)
+print("RGB:", r2, g2, b2)
+
